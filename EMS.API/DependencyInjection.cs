c49b33364a6163476cff691bde1d5be133dfa8ff
Sabbir Hasan <sabbir.hasan@technonext.com>
2024-11-10 17:16:33 +0600

    Register services and refactor program.cs

---
+++ EMS.API/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using EMS.Repository.Implementations;
+using EMS.Repository.Interfaces;
+using EMS.Repository.Models;
+using EMS.Services.Implementations;
+using EMS.Services.Interfaces;
+
+namespace EMS.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterUserServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Department>, DepartmentRepository>();
+            services.AddScoped<IRepository<Designation>, DesignationRepository>();
+            services.AddScoped<IRepository<Employee>, EmployeeRepository>();
+
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IDepartmentService, DepartmentService>();
+            services.AddScoped<IDesignationService, DesignationService>();
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterFrameworkServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
--- EMS.API/Program.cs
+++ EMS.API/Program.cs
@@ -1,11 +1,10 @@
+using EMS.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.RegisterUserServices();
+builder.Services.RegisterFrameworkServices();
 
 var app = builder.Build();
 
